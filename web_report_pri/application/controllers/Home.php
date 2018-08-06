e7d82acdf1443745e8fc78eadc537850c8fde933
sukan sittibamrungsuk <58160500@go.buu.ac.th>
2018-08-06 21:31:44 +0700

    set header & footer

--- web_report_pri/application/controllers/Home.php
+++ web_report_pri/application/controllers/Home.php
@@ -1,7 +1,14 @@
 <?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Home extends CI_Controller{
-     function index(){
-        $this->load->view('test_view.php');
+     public function index(){
+        $data['title'] = "HOME PAGE";
+        $this->load->view('header',$data);
+        $this->load->view('main_view');
+        $this->load->view('footer');
      }
+
+    
 }
 ?>
